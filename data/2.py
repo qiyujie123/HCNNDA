997534d12ee0515fd7274c3658b6dd831e6400e6
qi123 <147156566+qiyujie123@users.noreply.github.com>
2025-09-22 01:16:02 +0800

    Add files via upload

---
+++ data/2.py
@@ -0,0 +1,39 @@
+{
+        'data_dir': args.data_dir,
+        'lncRNA_view_files': [
+            'miRNA_seq_sim_scores_kmer.csv',
+            'mirna-exp-sim.csv',
+            'miRNA_go_sim.csv'
+        ],
+        'drug_view_files': [
+            'drug-feature-sim.csv',
+            'drug-lab-sim.csv',
+            'drug_similarity_matrix.csv'
+        ],
+        'adj_file': 'mirna_drug.csv'
+
+{
+        'data_dir': args.data_dir,
+        'lncRNA_view_files': [
+            'expression_out.csv',
+            'GO similarity_out.csv',
+            'k_mer_out.csv'
+        ],
+        'drug_view_files': [
+            'drug_feature_out.csv',
+            'drug_GIN.csv',
+            'weave_output_file.csv'
+        ],
+        'adj_file': 'mirna-drug.csv'
+cfg = {
+        'data_dir': args.data_dir,
+        'lncRNA_view_files': [
+            'circrna-seq-sim.csv',
+            'circ-exe-sim.csv',
+        ],
+        'drug_view_files': [
+            'Drug smiles sim.csv',
+            'Drug atc sim.csv',
+        ],
+        'adj_file': 'circ-association-matrix.csv'
+    }
